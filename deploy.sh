61595693ac75b303b97057844153343a41622f98
Gannon Barnett <gannonbarnett@gmail.com>
2023-02-12 19:08:54 -0800

    deploy

--- deploy.sh
+++
@@ -1,9 +0,0 @@
-rm -rf build
-python3 freeze.py
-echo "daysoffplanner.com" > build/CNAME
-git add .
-git commit -m "deploy"
-git push
-git subtree split --prefix build -b gh-pages 
-git push -f origin gh-pages:gh-pages
-git branch -D gh-pages 
